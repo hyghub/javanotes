5b342b88a6495943ea9529919c00ba36070c1a52
hyghub <745183282@qq.com>
2019-06-04 02:32:41 +0800

    --activemq 优化

---
+++ ActiveMQ/ActiveMQ-queue/src/main/java/com/touch/queue/ActiveMQUtil.java
@@ -0,0 +1,17 @@
+package com.touch.queue;
+
+import cn.hutool.core.util.NetUtil;
+
+import javax.swing.*;
+
+public class ActiveMQUtil {
+	public static void main(String[] args) {
+		checkServer();
+	}
+	public static void checkServer() {
+		if(NetUtil.isUsableLocalPort(8161)) {
+			JOptionPane.showMessageDialog(null, "ActiveMQ 服务器未启动 ");
+			System.exit(1);
+		}
+	}
+}
