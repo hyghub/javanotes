d3ae6ac2608ca4cae181e1a2b5880bf77d8389c4
hyghub <745183282@qq.com>
2019-06-04 01:52:24 +0800

    clear

--- springboot-jpa/src/main/java/com/how2java/springboot/dao/CategoryDAO.java
+++
@@ -1,9 +0,0 @@
-package com.how2java.springboot.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.how2java.springboot.pojo.Category;
-
-public interface CategoryDAO extends JpaRepository<Category,Integer>{
-
-}
