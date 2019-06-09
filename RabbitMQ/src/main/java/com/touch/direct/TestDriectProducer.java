3b8cfc8b759a0885b6f44d3bcc9e299a8497e8bb
hyghub <745183282@qq.com>
2019-06-09 22:57:04 +0800

    --RabbitMQ-fanout模式

---
+++ RabbitMQ/src/main/java/com/touch/direct/TestDriectProducer.java
@@ -0,0 +1,9 @@
+package com.touch.direct;
+
+/**
+ * 启动生产者，生产100条信息。
+ * 两个消费者分食 这100条信息 与ActiveMQ队列模式类似
+ */
+public class TestDriectProducer {
+
+}
---
+++ RabbitMQ/src/main/java/com/touch/topic/TestProducer.java
@@ -0,0 +1,12 @@
+package com.touch.topic;
+
+/**
+ * 四个路由："usa.news", "usa.weather", "europe.news", "europe.weather"
+ * 上发布 "美国新闻", "美国天气", "欧洲新闻", "欧洲天气"
+ * 于是就能在消费者端看到 不同的主题收到对应的消息了。
+ * 例如一个消费者是美国主题：能收到"美国新闻", "美国天气";
+ * 一个消费者是天气主题：能收到"美国天气","欧洲天气"
+ */
+public class TestProducer {
+
+}
