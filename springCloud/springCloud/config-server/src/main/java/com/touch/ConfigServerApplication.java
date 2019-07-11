9437dc7b35f464dbfcd9c7579ba547088bd187d3
runoob <test@runoob.com>
2019-07-11 19:54:27 +0800

    --ConfigServer 配置服务器

--- springCloud/springCloud/config-server/src/main/java/com/touch/ConfigServerApplication.java
+++ springCloud/springCloud/config-server/src/main/java/com/touch/ConfigServerApplication.java
@@ -20,6 +20,7 @@ public class ConfigServerApplication {
 			System.err.printf("端口%d被占用了，无法启动%n", port );
 			System.exit(1);
 		}
+
 		new SpringApplicationBuilder(ConfigServerApplication.class).properties("server.port=" + port).run(args);
     }
 }
