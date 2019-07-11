ecdd61d3408fe4830e599431a7b4e5d623da5577
runoob <test@runoob.com>
2019-07-11 19:59:12 +0800

    --product-data-service 数据微服务

--- springCloud/springCloud/product-data-service/src/main/java/com/touch/ProductDataServiceApplication.java
+++ springCloud/springCloud/product-data-service/src/main/java/com/touch/ProductDataServiceApplication.java
@@ -55,6 +55,7 @@ public class ProductDataServiceApplication {
         }
 
         new SpringApplicationBuilder(ProductDataServiceApplication.class).properties("server.port=" + port).run(args);
+
     }
     @Bean
     public Sampler defaultSampler() {
